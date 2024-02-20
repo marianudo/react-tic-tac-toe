5a822e2e4410da6de4a5bc8dcfa1ad4d458859fe
marianonavas <forceattack@protonmail.com>
2024-02-20 20:20:16 +0100

    Preliminary game layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,22 @@ function App() {
   
 
   return (
-    <p>Coming soon ...</p>
+    <main>
+      <div id="game-container">
+        <ol id="playes">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symol">O</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
   );
 }
 
