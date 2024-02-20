86a0357ed2716953b3d0bd881b8108ef18eb7097
marianonavas <forceattack@protonmail.com>
2024-02-20 21:34:57 +0100

    Edit player name, second iteration
    We do now two way binding between the name that is output and the value
    that is typed.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react';
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
 
     const [ isEditing, setIsEditing ] = useState(false);
+    const [ playerName, setPlayerName] = useState(initialName);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
     }
 
+    function handleNameChange(event) {
+        setPlayerName(event.target.value);
+    }
+
     return (
         <li>
             <span className="player">
-              {!isEditing && <span className="player-name">{name}</span>}
-              {isEditing && <input type='text' required value={name}/>}
+              {!isEditing && <span className="player-name">{playerName}</span>}
+              {isEditing && <input type='text' required value={playerName} onChange={handleNameChange}/>}
               <span className="player-symol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
